571924288cef52e94f9844d2e905db5bcf25b479
李冲 <lic-w@glodon.com>
2019-06-18 16:39:06 +0800

    影院热映、即将上映

--- lib/pages/movie/HotSoonMovieWidget.dart
+++ lib/pages/movie/HotSoonMovieWidget.dart
@@ -9,45 +9,76 @@ class HotSoonMovieWidget extends StatefulWidget {
 }
 
 TabController _tabController;
+var movieCount = 16;
 
 class _HotSoonMovieWidgetState extends State<HotSoonMovieWidget>
     with SingleTickerProviderStateMixin {
   Color selectColor, unselectedColor;
   TextStyle selectStyle, unselectedStyle;
+  Widget tabBar;
+  double childAspectRatio = 355.0 / 506.0;
 
   @override
   void initState() {
     super.initState();
     selectColor = Color.fromARGB(255, 45, 45, 45);
-    unselectedColor = Color.fromARGB(255, 117, 117, 117);
-    selectStyle = TextStyle(fontSize: 18, color: selectColor);
-    unselectedStyle = TextStyle(fontSize: 18, color: selectColor);
+    unselectedColor = Color.fromARGB(255, 135, 135, 135);
+    selectStyle = TextStyle(
+        fontSize: 20, color: selectColor, fontWeight: FontWeight.bold);
+    unselectedStyle = TextStyle(fontSize: 20, color: unselectedColor);
     _tabController = TabController(vsync: this, length: 2);
+    tabBar = TabBar(
+      tabs: [Text('影院热映'), Text('即将上映')],
+      indicatorColor: selectColor,
+      labelColor: selectColor,
+      labelStyle: selectStyle,
+      unselectedLabelColor: unselectedColor,
+      unselectedLabelStyle: unselectedStyle,
+      indicatorSize: TabBarIndicatorSize.label,
+      controller: _tabController,
+      isScrollable: true,
+      onTap: (index) {
+        setState(() {
+          if (index == 0) {
+            movieCount = 16;
+          } else {
+            movieCount = 20;
+          }
+        });
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TabBar(
-        tabs: [
-          Text(
-            '影院热映',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            '即将上映',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          )
-        ],
-        indicatorColor: selectColor,
-        labelColor: selectColor,
-        labelStyle: selectStyle,
-        unselectedLabelColor: unselectedColor,
-        unselectedLabelStyle: unselectedStyle,
-        indicatorSize: TabBarIndicatorSize.label,
-        controller: _tabController,
-        isScrollable: true,
-      ),
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: tabBar,
+              flex: 1,
+            ),
+            Text(
+              '全部 $movieCount > ',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+//        GridView.builder(
+//            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//                crossAxisCount: 3,
+//                crossAxisSpacing: 10.0,
+//                mainAxisSpacing: 10.0,
+//                childAspectRatio: childAspectRatio),
+//            //Widget Function(BuildContext context, int index);
+//            itemBuilder: (BuildContext context, int index) {
+//
+//            })
+      ],
     );
   }
 
--- lib/pages/movie/MoviePage.dart
+++ lib/pages/movie/MoviePage.dart
@@ -4,6 +4,7 @@ import 'package:flutter_demo/pages/movie/TodayPlayMovieWidget.dart';
 
 import 'HotSoonMovieWidget.dart';
 
+///书影音-电影
 class MoviePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -12,12 +13,13 @@ class MoviePage extends StatefulWidget {
 }
 
 class _MoviePageState extends State<MoviePage> {
-  Widget titleWidget, todayPlayMovieWidget;
+  Widget titleWidget, todayPlayMovieWidget, hotSoonMovieWidget;
 
   @override
   void initState() {
     super.initState();
     titleWidget = TitleWidget();
+    hotSoonMovieWidget = HotSoonMovieWidget();
     todayPlayMovieWidget = TodayPlayMovieWidget([
       'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p792776858.webp',
       'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p1374786017.webp',
@@ -40,9 +42,12 @@ class _MoviePageState extends State<MoviePage> {
             ),
             Padding(
               child: todayPlayMovieWidget,
+              padding: EdgeInsets.only(top: 22.0),
+            ),
+            Padding(
+              child: hotSoonMovieWidget,
               padding: EdgeInsets.only(top: 25.0),
             ),
-            HotSoonMovieWidget()
           ]))
         ],
       ),
