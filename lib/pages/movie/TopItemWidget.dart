54af968a2f2e7a541099793acf704cb9d0f2ae26
李冲 <lic-w@glodon.com>
2019-06-22 18:50:30 +0800

    豆瓣榜单

--- lib/pages/movie/TopItemWidget.dart
+++ lib/pages/movie/TopItemWidget.dart
@@ -6,11 +6,17 @@ import 'package:flutter_demo/widgets/image/CachedNetworkImage.dart';
 TopItemBean _bean;
 
 ///豆瓣榜单Item
+///
+@immutable
 class TopItemWidget extends StatefulWidget {
-  final state = _TopItemWidgetState();
+  _TopItemWidgetState state;
+  final title;
+
+  TopItemWidget(this.title, {Key key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
+    state = _TopItemWidgetState(title);
     return state;
   }
 
@@ -22,6 +28,11 @@ class TopItemWidget extends StatefulWidget {
 var _imgSize;
 
 class _TopItemWidgetState extends State<TopItemWidget> {
+  var title;
+  TopItemBean _bean;
+
+  _TopItemWidgetState(this.title);
+
   @override
   Widget build(BuildContext context) {
     if (_bean == null) {
@@ -32,9 +43,9 @@ class _TopItemWidgetState extends State<TopItemWidget> {
     }
 
     return Container(
-      color: Colors.red,
       width: _imgSize,
       height: _imgSize,
+      padding: EdgeInsets.only(top: 5.0, right: 10.0, bottom: 5.0),
       child: ClipRRect(
         borderRadius: BorderRadius.all(Radius.circular(5.0)),
         child: Stack(
@@ -44,6 +55,33 @@ class _TopItemWidgetState extends State<TopItemWidget> {
               height: _imgSize,
               fit: BoxFit.cover,
               imageUrl: _bean.imgUrl,
+            ),
+            Positioned(
+              top: 8.0,
+              right: 15.0,
+                child: Text(
+              _bean.count,
+              style: TextStyle(fontSize: 12.0, color: Colors.white),
+            )),
+            Positioned(
+              top: _imgSize / 2 - 40.0,
+              left: 30.0,
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Positioned(
+              top: _imgSize / 2,
+              child: Container(
+                height: _imgSize / 2,
+                width: _imgSize,
+                color: Colors.brown,
+              ),
             )
           ],
         ),
